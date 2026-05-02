c72c93089a48d61837dd39d05d558b7033c5815d
Rohit Verma <rv17837@outlook.com>
2026-05-02 13:38:16 +0530

    chore: update BASE_URL and configure HTTP cookies

--- composeApp/src/commonMain/kotlin/app/multiplatform/commons/network/HttpClient.kt
+++ composeApp/src/commonMain/kotlin/app/multiplatform/commons/network/HttpClient.kt
@@ -4,15 +4,21 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.plugins.DefaultRequest
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.cookies.AcceptAllCookiesStorage
+import io.ktor.client.plugins.cookies.HttpCookies
 import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
-private const val BASE_URL = "https://commons.wikimedia.beta.wmflabs.org/w/api.php?format=json&formatversion=2&errorformat=plaintext&"
+private const val BASE_URL = "https://commons.wikimedia.beta.wmcloud.org/"
 fun createHttpClient(engine: HttpClientEngine): HttpClient = HttpClient(engine) {
     install(DefaultRequest) {
         url(BASE_URL)
+    }
+    install(HttpCookies) {
+        storage = AcceptAllCookiesStorage()
     }
     install(ContentNegotiation) {
         json(Json {
@@ -22,5 +28,10 @@ fun createHttpClient(engine: HttpClientEngine): HttpClient = HttpClient(engine) 
     }
     install(Logging) {
         level = LogLevel.ALL
+        logger = object : Logger {
+            override fun log(message: String) {
+                println("KtorLogger: $message")
+            }
+        }
     }
 }
